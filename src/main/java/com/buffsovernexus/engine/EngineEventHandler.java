df0eae9b6e3b5d603db6cb40dcd4ba5e422803aa
rgoldbe4 <rgoldbe4@uncc.edu>
2021-07-06 17:48:31 -0400

    Added NotImplementedException to the project

--- src/main/java/com/buffsovernexus/engine/EngineEventHandler.java
+++ src/main/java/com/buffsovernexus/engine/EngineEventHandler.java
@@ -1,9 +1,12 @@
 package com.buffsovernexus.engine;
 
-import com.buffsovernexus.engine.EngineListener;
 import com.buffsovernexus.engine.event.GuardScoreEvent;
+import org.apache.commons.lang.NotImplementedException;
+
 
 public abstract class EngineEventHandler implements EngineListener {
     @Override
-    public void onGuardScoreEvent(GuardScoreEvent event) { }
+    public void onGuardScoreEvent(GuardScoreEvent event) {
+        throw new NotImplementedException("GuardScoreEvent has no listeners.");
+    }
 }
