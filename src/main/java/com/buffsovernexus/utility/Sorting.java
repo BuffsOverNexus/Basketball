80c0b9d75c3d96f7218b7acdb6cac21dff93cb69
BuffsOverNexus <buffsovernexus2@gmail.com>
2023-04-24 17:28:57 -0400

    Reversed order of sorting teams to properly reflect standings.

--- src/main/java/com/buffsovernexus/utility/Sorting.java
+++ src/main/java/com/buffsovernexus/utility/Sorting.java
@@ -4,10 +4,11 @@ import com.buffsovernexus.entity.SeasonTeam;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Sorting {
 
     public static void sortSeason(List<SeasonTeam> seasonTeams) {
-        seasonTeams.sort(Comparator.comparingDouble(SeasonTeam::getPercentage));
+        seasonTeams.sort(Comparator.comparingDouble(SeasonTeam::getPercentage).reversed());
     }
 }
